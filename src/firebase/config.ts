5b9b356e3c6b48fd64de6ac1cf0a9e8522a54330
عبد السلام <abozbere2001@gmail.com>
2025-10-26 10:32:02 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,12 +5,13 @@ import { getFirestore, type Firestore } from 'firebase/firestore';
 
 // Configuration object for Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyB1B2NlQyUNac6p_c0yZ0u-9vjFCoTqJjI",
+  apiKey: "AIzaSyDKQK4mfCGlSCwJS7oOdMhJa0SIJAv3nXM",
   authDomain: "nabd-d71ab.firebaseapp.com",
   projectId: "nabd-d71ab",
   storageBucket: "nabd-d71ab.appspot.com",
-  messagingSenderId: "338879612343",
-  appId: "1:338879612343:web:076f7f329c3d4b68453472"
+  messagingSenderId: "529236633123",
+  appId: "1:529236633123:web:7d4945daae4d51038e3396",
+  measurementId: "G-X5SY2K798F"
 };
 
 interface FirebaseServices {
